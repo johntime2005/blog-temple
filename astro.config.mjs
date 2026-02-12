d7cbdd9aab40c4032ac6491236a3476b16b789ec
GitHub Action <action@github.com>
2026-02-12 02:25:02 +0000

    Sync content from private repository [skip ci]

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
-import react from "@astrojs/react";
-import keystatic from "@keystatic/astro";
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 import swup from "@swup/astro";
@@ -71,7 +70,6 @@ export default defineConfig({
 				return event.state?.url?.includes("#");
 			},
 		}),
-		keystatic(),
 		react(),
 		icon({
 			include: {
